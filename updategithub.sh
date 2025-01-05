3e8104c17a00843fc238d8c720c94f2bcc315fad
baqi2 <bobwu1987@gmail.com>
2025-01-05 20:37:21 +0800

    更新

--- updategithub.sh
+++ updategithub.sh
@@ -1,3 +1,5 @@
+cd D:\BaiduSyncdisk\04-toursite
+
 git config --global user.name "baqi2"
 git config --global user.email "bobwu1987@gmail.com"
 
@@ -5,4 +7,9 @@ git remote set-url origin https://github.com/baqi2/testtourweb.github.io.git
 
 git add .
 git commit -m "更新"
-git push origin master
+
+# 将 master 分支改名为 main
+git branch -M main
+
+# 推送
+git push -u origin main
