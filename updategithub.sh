c7e5758d970e261d405cb2aef8987a4a2b39323e
baqi2 <bobwu1987@gmail.com>
2025-01-05 20:35:25 +0800

    更新

---
+++ updategithub.sh
@@ -0,0 +1,8 @@
+git config --global user.name "baqi2"
+git config --global user.email "bobwu1987@gmail.com"
+
+git remote set-url origin https://github.com/baqi2/testtourweb.github.io.git
+
+git add .
+git commit -m "更新"
+git push origin master
